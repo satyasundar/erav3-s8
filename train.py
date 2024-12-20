eda8032e5eab281fe4c03746f1cb6ec5297fb631
satyasundar <satyasundar.nayak@gmail.com>
2024-12-20 21:42:06 +0530

    update

--- train.py
+++ train.py
@@ -53,17 +53,17 @@ train_transforms = A.Compose(
     [
         A.HorizontalFlip(p=0.5),
         A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.1, rotate_limit=15, p=0.1),
-        # A.CoarseDropout(
-        #     max_holes=1,
-        #     max_height=16,
-        #     max_width=16,
-        #     min_holes=1,
-        #     min_height=16,
-        #     min_width=16,
-        #     fill_value=(0.4914, 0.4822, 0.4465),
-        #     mask_fill_value=None,
-        #     p=0.1,
-        # ),
+        A.CoarseDropout(
+            max_holes=1,
+            max_height=16,
+            max_width=16,
+            min_holes=1,
+            min_height=16,
+            min_width=16,
+            fill_value=(0.4914, 0.4822, 0.4465),
+            mask_fill_value=None,
+            p=0.1,
+        ),
         A.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)),
         ToTensorV2(),
     ]
